4404afebe3baf60e0716d9db2466f28fbbbce0b4
broche079 <lary950709@gmail.com>
2025-10-23 17:11:16 +0800

    week07

---
+++ week07/week07-2.cpp
@@ -0,0 +1,21 @@
+///week07-2.cpp 像畫星星一樣
+///畫出超大的正方形 數字包起來
+///TAICA 交大基礎程式設計(C++) 期中考考題第6題
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int n;///Part 1: Input
+    cin >> n;
+
+    for(int i=1; i<n*2; i++){ ///Part 2:Output
+        for(int j=1; j<n*2; j++){
+            ///印出來之後 發現正中心的1很有意思/有點東西!!!
+            int d = max(abs(i-n), abs(j-n));
+            cout << d +1;///cout << n
+        }
+        cout << endl;///cout << "現在i是:" <<
+    }
+}///2會有3層樓
+
